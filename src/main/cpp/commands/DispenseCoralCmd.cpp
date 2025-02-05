0a9d68f4770d6584ec1f1e5212e2a7b196db263b
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2025-02-04 17:47:37 -0700

    Skyler Sebold - Finished Dispense Coral Cmd
    That

--- src/main/cpp/commands/DispenseCoralCmd.cpp
+++ src/main/cpp/commands/DispenseCoralCmd.cpp
@@ -1,1 +1,19 @@
 #include "commands/DispenseCoralCmd.hpp"
+
+DispenseCoralCmd::DispenseCoralCmd()
+{
+
+}
+
+DispenseCoralCmd::~DispenseCoralCmd()
+{
+
+}
+
+frc2::CommandPtr DispenseCoralCmd::DispenseCoralCmdFunction(int CoralLevel)
+{
+    //CoralLevel = Level to put coral on reef 
+   std::move(m_pivotSubsystem.SetAngle(PivotConstants::kCoralAngle)).AndThen(std::move(m_elevatorSubsystem.MoveLevel(CoralLevel))).AndThen(std::move(m_guacoSubsystem.Dispense(GuacoConstants::kSpeed)));
+    
+}
+
