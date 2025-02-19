b91ac411db4d344ffb17c79a11b792beb3d0f889
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2025-02-18 19:29:41 -0700

    Briscoe McCune
    Most of the code for vision has been created. The name of the camera and the other constants in the camera class need to be worked on more. This code as of this day is untested.

--- src/main/cpp/subsystems/Camera.cpp
+++ src/main/cpp/subsystems/Camera.cpp
@@ -1,1 +1,183 @@
 #include "subsystems/Camera.hpp"
+
+namespace Robot2025
+{
+
+    // Constructor:
+    Camera::Camera() {}
+
+    // Target Values (Change each season):
+    units::meter_t Camera::AprilTagHeightMeters(int aprilTagID)
+    {
+        // Grab apriltag height in meters.
+		// Note: Height = center.
+
+		// Variables.
+        units::meter_t aprilTagHeight = 0.0_m;
+		units::meter_t coralStation = 1.485_m;
+		units::meter_t processor = 1.305_m;
+		units::meter_t reef = 0.305_m;
+		units::meter_t barge = 1.915_m;
+
+		// Set height depending on ID.
+		switch (aprilTagID)
+		{
+		case 1:
+		case 2:
+			aprilTagHeight = coralStation;
+			break;
+
+		case 3:
+			aprilTagHeight = processor;
+			break;
+
+		case 4:
+		case 5:
+			aprilTagHeight = barge;
+			break;
+
+		case 6:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			aprilTagHeight = reef;
+			break;
+
+		case 12:
+		case 13:
+			aprilTagHeight = coralStation;
+			break;
+
+		case 14:
+		case 15:
+			aprilTagHeight = barge;
+			break;
+
+		case 16:
+			aprilTagHeight = processor;
+			break;
+
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+		case 21:
+		case 22:
+			aprilTagHeight = reef;
+			break;
+
+		default:
+			break;
+		}
+
+		return 	aprilTagHeight;
+    }
+
+    std::string Camera::AprilTagGameLocation(int aprilTagID)
+    {
+        // Grab apriltag game location name.
+
+		// Variables.
+        std::string aprilTagGameLocation = " ";
+		std::string coralStation = "Coral Station";
+		std::string processor = "Processor";
+		std::string reef = "Reef";
+		std::string barge = "Barge";
+
+		// Set game location name depending on ID.
+		switch (aprilTagID)
+		{
+		case 1:
+		case 2:
+			aprilTagGameLocation = coralStation;
+			break;
+
+		case 3:
+			aprilTagGameLocation = processor;
+			break;
+
+		case 4:
+		case 5:
+			aprilTagGameLocation = barge;
+			break;
+
+		case 6:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			aprilTagGameLocation = reef;
+			break;
+
+		case 12:
+		case 13:
+			aprilTagGameLocation = coralStation;
+			break;
+
+		case 14:
+		case 15:
+			aprilTagGameLocation = barge;
+			break;
+
+		case 16:
+			aprilTagGameLocation = processor;
+			break;
+
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+		case 21:
+		case 22:
+			aprilTagGameLocation = reef;
+			break;
+
+		default:
+			break;
+		}
+
+		return 	aprilTagGameLocation;
+    }
+
+    // Periodic Function:
+    void Camera::ProcessCamAprilTags()
+    {
+        photon::PhotonPipelineResult camResult = m_camera.GetLatestResult();
+
+        if(camResult.HasTargets() == false)
+        {
+            return;
+        }
+
+        m_aprilTagTargets = camResult.GetTargets();
+        unsigned int aprilTagArraySize = m_aprilTagTargets.size();
+
+        for (int i = 0; i < aprilTagArraySize; i++)
+        {
+            photon::PhotonTrackedTarget target = m_aprilTagTargets[i];
+            
+            int targetID = target.GetFiducialId();
+            units::meter_t targetHeightMeters = AprilTagHeightMeters(targetID);
+
+            double targetHeightDouble = targetHeightMeters.value();
+            std::string targetGameLocation = AprilTagGameLocation(targetID);
+
+            units::meter_t targetDistMeters = photon::PhotonUtils::CalculateDistanceToTarget(m_camHeight, targetHeightMeters, m_camPitch, m_targetPitch);
+            double targetDistDouble = targetDistMeters.value();
+
+            AprilTag curTag;
+            curTag.SetID(targetID);
+            curTag.SetDistance(targetDistDouble);
+            curTag.SetHeight(targetHeightDouble);
+            curTag.SetYaw(target.GetYaw());
+            curTag.SetPitch(target.GetPitch());
+            curTag.SetSkew(target.GetSkew());
+            curTag.SetGameLocation(targetGameLocation);
+        }
+    }
+
+    // Destructor:
+    Camera::~Camera() {}
+
+}
--- src/main/include/subsystems/Camera.hpp
+++ src/main/include/subsystems/Camera.hpp
@@ -1,4 +1,14 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <photon/PhotonCamera.h>
+#include <photon/PhotonUtils.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <units/angle.h>
+#include <units/length.h>
+
+#include "AprilTag.hpp"
+#include "Constants.hpp"
 
 namespace Robot2025
 {
@@ -6,12 +16,30 @@ namespace Robot2025
 class Camera
 {
     public:
+    // Constructor:
+    Camera();
+    
+    // Periodic Function:
+    void ProcessCamAprilTags();
+
+    // Destructor:
+    ~Camera();
 
     private:
+    // Initialize Camera(s)
+    photon::PhotonCamera m_camera{"kCamName (Change)"};
+    std::span<const photon::PhotonTrackedTarget, 4294967295U> m_aprilTagTargets;
 
-    
+    units::meter_t m_targetDist = 0.0_m;
+    units::meter_t m_camHeight = 0.0_m;
+    units::radian_t m_camPitch = 0.0_rad;
+    units::radian_t m_targetPitch = 0.0_rad;
+
+    std::vector<AprilTag> m_aprilTags;
+
+    // Functions:
+    units::meter_t AprilTagHeightMeters(int aprilTagID);
+    std::string AprilTagGameLocation(int aprilTagID);
 };
-
-
 
 }
