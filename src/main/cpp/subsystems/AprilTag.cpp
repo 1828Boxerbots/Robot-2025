95640a70fba07d1a54b522fb73579f4783b16288
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2025-02-22 10:16:51 -0700

    Briscoe McCune
    Added Photonvision code (the camera and apriltag classes under subsystem header) for availability for test.

--- src/main/cpp/subsystems/AprilTag.cpp
+++ src/main/cpp/subsystems/AprilTag.cpp
@@ -1,7 +1,92 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+#include "subsystems/AprilTag.hpp"
 
-#include "subsystems/AprilTag.h"
+namespace Robot2025
+{
 
-AprilTag::AprilTag() = default;
+    	// Setter Functions:
+		void AprilTag::SetID(const int& aprilTagID)
+        {
+            // Set apriltag ID.
+		    m_aprilTagID = aprilTagID;
+		    return;
+        }
+
+		void AprilTag::SetDistance(const double& aprilTagDist)
+        {
+            // Set apriltag distance.
+            m_aprilTagDist = aprilTagDist;
+            return;
+        }
+
+        void AprilTag::SetHeight(const double& aprilTagHeight)
+        {
+            // Set apriltag height.
+            m_aprilTagHeight = aprilTagHeight;
+            return;
+        }
+
+        void AprilTag::SetYaw(const int& aprilTagYaw)
+        {
+            // Set apriltag Yaw.
+            m_aprilTagYaw = aprilTagYaw;
+            return;
+        }
+
+		void AprilTag::SetPitch(const double& aprilTagPitch)
+        {
+            // Set apriltag pitch.
+            m_aprilTagPitch = aprilTagPitch;
+            return;    
+        }
+
+        void AprilTag::SetSkew(const double& aprilTagSkew)
+        {
+            // Set apriltag skew.
+            m_aprilTagSkew = aprilTagSkew;
+            return;
+        }
+
+        void AprilTag::SetGameLocation(const std::string& aprilTagGameLocation)
+        {
+            // Set apriltag game location.
+            m_aprilTagGameLocation = aprilTagGameLocation;
+            return;
+        }
+
+        // Get Data Functions:
+        int AprilTag::GetID()
+        {
+            return m_aprilTagID;
+        }
+
+        double AprilTag::GetDistMeters()
+        {
+            return m_aprilTagDist;
+        }
+
+        double AprilTag::GetHeightMeters()
+        {
+            return m_aprilTagHeight;
+        }
+
+        double AprilTag::GetYawDegrees()
+        {
+            return m_aprilTagYaw;
+        }
+
+        double AprilTag::GetPitchDegrees()
+        {
+            return m_aprilTagPitch;
+        }
+
+        double AprilTag::GetSkewDegrees()
+        {
+            return m_aprilTagSkew;
+        }
+
+        std::string AprilTag::GetGameLocation()
+        {
+            return m_aprilTagGameLocation;
+        }
+
+}
--- src/main/include/subsystems/Camera.hpp
+++ src/main/include/subsystems/Camera.hpp
@@ -1,4 +1,15 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <photon/PhotonCamera.h>
+#include <photon/PhotonUtils.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <units/angle.h>
+#include <units/length.h>
+#include <memory.h>
+
+#include "AprilTag.hpp"
+#include "Constants.hpp"
 
 namespace Robot2025
 {
@@ -6,12 +17,32 @@ namespace Robot2025
 class Camera
 {
     public:
+    // Constructor:
+    Camera(const std::string& camName = "BoxerCam1");
+    
+    // Periodic Function:
+    void ProcessCamAprilTags();
+
+    // Data Function:
+    AprilTag AprilTagsData(int member);
+    std::vector<AprilTag> AprilTagsDataArray();
+
+    // Destructor:
+    ~Camera();
 
     private:
+    // Initialize Camera(s)
+    std::unique_ptr<photon::PhotonCamera> m_camera;
 
-    
+    units::meter_t m_camHeightMeters = 0.0_m; //Camera height in meters from center of lens to floor.
+    units::radian_t m_camPitchDegrees = 0.0_deg; //Camera pitch in degrees. 
+    units::radian_t m_targetPitchDegrees = 0.0_deg; //Apriltag pitch in degrees.
+
+    std::vector<AprilTag> m_aprilTags;
+
+    // Functions:
+    units::meter_t AprilTagHeightMeters(int aprilTagID);
+    std::string AprilTagGameLocation(int aprilTagID);
 };
-
-
 
 }
